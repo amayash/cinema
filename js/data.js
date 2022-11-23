192253d48fb2c735b283e352d84a1fcf8388e309
dasha <amayashimizu89@gmail.com>
2022-11-24 00:09:15 +0400

    add pug

---
+++ js/data.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+    return {
+        items: [
+            'Ноутбук',
+            'Телефон',
+            'Приставка'
+        ],
+        lines: [
+            {id: 1, name: 'Ноутбук', price: "59990.00", count: 2, sum: "119980.00"}
+        ]
+    }
+}
